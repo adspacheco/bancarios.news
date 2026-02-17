0d37e8b0d8438e2e13bbcfeef61ea3b22c42cd44
adspacheco <andersonac7@outlook.com>
2026-02-17 12:06:33 -0300

    feat: invalidate session cookie on unauthorized error

--- infra/controller.js
+++ infra/controller.js
@@ -31,11 +31,12 @@ function onNoMatchHandler(request, response) {
  * @param {import("http").ServerResponse} response
  */
 function onErrorHandler(error, request, response) {
-  if (
-    error instanceof ValidationError ||
-    error instanceof NotFoundError ||
-    error instanceof UnauthorizedError
-  ) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
+    return response.status(error.statusCode).json(error);
+  }
+
+  if (error instanceof UnauthorizedError) {
+    clearSessionCookie(response);
     return response.status(error.statusCode).json(error);
   }
 
